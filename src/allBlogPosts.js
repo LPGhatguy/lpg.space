b7a64d1ae14039b2762134790571a7fe8519fffd
Lucien Greathouse <me@lpghatguy.com>
2021-10-23 15:32:25 -0400

    New post, old post, draft system

--- src/allBlogPosts.js
+++ src/allBlogPosts.js
@@ -1,21 +1,42 @@
 import postMap from "./posts/*.mdx";
 
+function parseSlug(slug) {
+	const fullMatches = slug.match(/^(\d+)-(\d+)-(\d+)-(.+)$/);
+	if (fullMatches != null) {
+		const year = Number(fullMatches[1]);
+		const month = Number(fullMatches[2]);
+		const day = Number(fullMatches[3]);
+		const id = fullMatches[4];
+
+		return { draft: false, id, date: new Date(year, month - 1, day) }
+	}
+
+	const draftMatches = slug.match(/^draft-(.+)$/);
+	if (draftMatches != null) {
+		return { draft: true, id: draftMatches[1], date: new Date() };
+	}
+
+	throw new Error("Invalid blog post slug: " + slug);
+}
+
 const posts = [];
 
 for (const [slug, {metadata, default: Body}] of Object.entries(postMap)) {
-	const { title, hidden } = metadata;
-    const [_, yearStr, monthStr, dayStr, id] = slug.match(/^(\d+)-(\d+)-(\d+)-(.+)$/);
-	const year = Number(yearStr);
-	const month = Number(monthStr);
-	const day = Number(dayStr);
+	let { title, hidden } = metadata;
+	const { id, date, draft } = parseSlug(slug);
+
+	if (draft) {
+		title = "DRAFT: " + title;
+		hidden = true;
+	}
 
     posts.push({
 		slug,
 		title,
 		hidden,
 		id,
+		date,
 		Body,
-		date: new Date(year, month - 1, day),
 	});
 }
 
